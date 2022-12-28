7a20a980d2cb4ce569510f0a80e04006bc2445dd
Mduletsie7 <mdu.letsie7@gmail.com>
2022-12-28 15:34:56 +0200

    Added Add to cart button on products container

--- src/components/Store.js
+++ src/components/Store.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import '../Store.css'
+import { FaShoppingBag } from 'react-icons/fa';
 
 function Store() {
   var storeItem = [];
-  storeItem.push("Goku Turtle Hermit Gi", 20.00, "4 Left in stock");
+  storeItem.push("0001", "Goku Turtle Hermit Gi", 20.00, "Jacket");
+  console.log(storeItem[0])
 
 
     return (
@@ -16,11 +18,11 @@ function Store() {
       <div class="product-image">
       <img className='clothingItem' src={process.env.PUBLIC_URL + "/images/zstore/blackjacket.jpg"} width="300" alt='Redditz black drip jacket'/>
       </div>
-      <div class="product-sku"> '. $row["sku"].' </div>
-      <div class="product-name"> '. $row["name"].' </div>
-      <div class="product-price"> '. $row["price"].' </div>
-      <div class="product-type"> '. $row["type"].' </div>
-      <div class="product-value"> '. $row["value"].' </div>
+      <div class="product-sku"> SKU: {storeItem[0]} </div>
+      <div class="product-name"> Name: {storeItem[1]} </div>
+      <div class="product-price"> Price: ${storeItem[2]} </div>
+      <div class="product-type"> Type: {storeItem[3]} </div>
+      <div class="addProduct"><FaShoppingBag/> </div>
     </div>
       </div>
       
